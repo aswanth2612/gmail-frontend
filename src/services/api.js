dc8c3de468e23350600084fef781d1ffd8c20258
aswanth2612 <aswanthmmg@gmail.com>
2024-03-21 12:27:19 +0530

    commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const API_URL = 'https://gmail-frontend-ektac0wev-aswanths-projects.vercel.app/';
+const API_URL = 'http://localhost:8000';
 
 const API_GMAIL = async (urlObject, payload, type) => {
     return await axios({
