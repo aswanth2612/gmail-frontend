b7f19684ecdac3bc884ad4ba76e617c47948bfa3
aswanth2612 <aswanthmmg@gmail.com>
2024-03-23 21:42:16 +0530

    import.meta.env.VITE_APP_

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const API_GMAIL = async (urlObject, payload, type) => {
-    console.log(process.env.REACT_APP_API_URL + urlObject.endpoint + type);
+    console.log(import.meta.env.VITE_APP_API_URL + urlObject.endpoint + type);
     return await axios({
         method: urlObject.method,
-        url: `${process.env.API_URL}/${urlObject.endpoint}/${type}`,
+        url: `${import.meta.env.VITE_APP_API_URL}/${urlObject.endpoint}/${type}`,
         data: payload
     })
 }
