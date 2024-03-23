da9a2c1a7f82254c61665670f6ff5597ad1171fb
aswanth2612 <aswanthmmg@gmail.com>
2024-03-23 21:34:10 +0530

    React script added

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,4 @@
 import axios from 'axios';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const API_GMAIL = async (urlObject, payload, type) => {
     console.log(process.env.API_URL + urlObject.endpoint + type);
