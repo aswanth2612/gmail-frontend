47b963949fb9b23158e39a218cd9b706793e2240
aswanth2612 <aswanthmmg@gmail.com>
2024-03-23 21:12:20 +0530

    Env data received

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 
-const API_URL = process.env.API_URL;
+const API_URL = `${process.env.API_URL}`;
 
 const API_GMAIL = async (urlObject, payload, type) => {
+    console.log(API_URL + urlObject.endpoint + type);
     return await axios({
         method: urlObject.method,
         url: `${API_URL}/${urlObject.endpoint}/${type}`,
