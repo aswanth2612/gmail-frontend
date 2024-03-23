424b964b4044ea0fff34c094465a77783d8d40af
aswanth2612 <aswanthmmg@gmail.com>
2024-03-23 21:39:48 +0530

    REACT_APP_

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_GMAIL = async (urlObject, payload, type) => {
-    console.log(process.env.API_URL + urlObject.endpoint + type);
+    console.log(process.env.REACT_APP_API_URL + urlObject.endpoint + type);
     return await axios({
         method: urlObject.method,
         url: `${process.env.API_URL}/${urlObject.endpoint}/${type}`,
