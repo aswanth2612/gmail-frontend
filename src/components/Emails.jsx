2e485becea24e6fa7f74dcf5b6ab336db41272c9
aswanth2612 <aswanthmmg@gmail.com>
2024-04-14 23:41:51 +0530

    Alignments

--- src/components/Emails.jsx
+++ src/components/Emails.jsx
@@ -12,83 +12,86 @@ import axios from 'axios';
 import { useCookies } from 'react-cookie';
 
 const Emails = ({ state }) => {
-    const currentUser = useUser();
-    const navigate = useNavigate();
-    const [cookies, setCookie, removeCookie] = useCookies(['username', 'email', 'token']);
-    const [selectedEmails, setSelectedEmails] = useState([]);
-    const [refreshScreen, setRefreshScreen] = useState(false);
-    const { openDrawer } = useOutletContext();
-    const params = useParams();
-    const type = params.type;
+  const currentUser = useUser();
+  const navigate = useNavigate();
+  const [cookies, setCookie, removeCookie] = useCookies(['username', 'email', 'token']);
+  const [selectedEmails, setSelectedEmails] = useState([]);
+  const [refreshScreen, setRefreshScreen] = useState(false);
+  const { openDrawer } = useOutletContext();
+  const params = useParams();
+  const type = params.type;
 
-    const getEmailServices = useApi(API_URLS.getEmailFromType);
-    const moveEmailsToBinService = useApi(API_URLS.moveEmailsToBin);
-    const deleteEmailService = useApi(API_URLS.deleteEmail);
+  const getEmailServices = useApi(API_URLS.getEmailFromType);
+  const moveEmailsToBinService = useApi(API_URLS.moveEmailsToBin);
+  const deleteEmailService = useApi(API_URLS.deleteEmail);
 
-    useEffect(() => {
-        getEmailServices.call({ to: cookies.email }, type);
-        axios.defaults.withCredentials = true;
-        axios.defaults.headers.common['Authorization'] = 'Bearer ' + cookies.token;
-        axios.get(import.meta.env.VITE_BACKEND_PATH + '/auth/verify', {
-            params: { token: cookies.token },
-            data: { token: cookies.token },
-        }).then(res => {
-            if (res.data.status) {
-            } else {
-                removeCookie("username");
-                removeCookie("email");
-                removeCookie("token");
-                navigate('/login')
-            }
-        }).catch(() => {
-            navigate("/error");
-        });
-    }, [type, refreshScreen])
+  useEffect(() => {
+    getEmailServices.call({ to: cookies.email }, type);
+    axios.defaults.withCredentials = true;
+    axios.defaults.headers.common['Authorization'] = 'Bearer ' + cookies.token;
+    axios.get(import.meta.env.VITE_BACKEND_PATH + '/auth/verify', {
+      params: { token: cookies.token },
+      data: { token: cookies.token },
+    }).then(res => {
+      if (res.data.status) {
+      } else {
+        removeCookie("username");
+        removeCookie("email");
+        removeCookie("token");
+        navigate('/login')
+      }
+    }).catch(() => {
+      navigate("/error");
+    });
+  }, [type, refreshScreen])
 
-    const selectAllEmails = (e) => {
-        if (e.target.checked) {
-            const emails = getEmailServices ?.response ?.map(email => email._id);
-            setSelectedEmails(emails);
-        } else {
-            setSelectedEmails([]);
-        }
+  const selectAllEmails = (e) => {
+    if (e.target.checked) {
+      const emails = getEmailServices ?.response ?.map(email => email._id);
+      setSelectedEmails(emails);
+    } else {
+      setSelectedEmails([]);
     }
+  }
 
-    const deleteSelectedEmails = (e) => {
-        if (type === 'bin') {
-            deleteEmailService.call(selectedEmails);
-        } else {
-            moveEmailsToBinService.call(selectedEmails);
-        }
-        setRefreshScreen(prevState => !prevState)
+  const deleteSelectedEmails = (e) => {
+    if (type === 'bin') {
+      deleteEmailService.call(selectedEmails);
+    } else {
+      moveEmailsToBinService.call(selectedEmails);
     }
+    setRefreshScreen(prevState => !prevState)
+  }
 
-    return (
-        <Box style={openDrawer ? { marginLeft: 250, width: 'calc(100% - 250px)' } : { width: '100%' }}>
+  return (
+    <Box style={openDrawer ? { marginLeft: 250, width: 'calc(100% - 250px)' } : { width: '100%' }}>
 
-            <Box style={{ padding: '20px 10px 0 10px', display: 'flex', alignItems: 'center' }}>
-                <Checkbox size="small" onChange={(e) => selectAllEmails(e)} />
-                <DeleteOutlined onClick={(e) => deleteSelectedEmails(e)} />
-            </Box>
-            <List>
-                {
-                    getEmailServices ?.response ?.map(email => (
-                        <Email
-                            key={email._id}
-                            email={email}
-                            selectedEmails={selectedEmails}
-                            setRefreshScreen={setRefreshScreen}
-                            setSelectedEmails={setSelectedEmails}
-                        />
-                    ))
+      <Box style={{ padding: '20px 10px 0 10px', display: 'flex', alignItems: 'center' }}>
+        {
+          getEmailServices ?.response ?.length > 0 &&
+            <><Checkbox size="small" onChange={(e) => selectAllEmails(e)} />
+              <DeleteOutlined onClick={(e) => deleteSelectedEmails(e)} />
+            </>}
+      </Box>
+      <List>
+        {
+          getEmailServices ?.response ?.map(email => (
+            <Email
+              key={email._id}
+              email={email}
+              selectedEmails={selectedEmails}
+              setRefreshScreen={setRefreshScreen}
+              setSelectedEmails={setSelectedEmails}
+            />
+          ))
                  }
-            </List>
-            {
-                getEmailServices ?.response ?.length === 0 &&
-                    <NoMails message={EMPTY_TABS[type]} />
+      </List>
+      {
+        getEmailServices ?.response ?.length === 0 &&
+          <NoMails message={EMPTY_TABS[type]} />
              }
-        </Box>
-    )
+    </Box>
+  )
 }
 
 export default Emails;
--- src/components/common/NoMails.jsx
+++ src/components/common/NoMails.jsx
@@ -1,16 +1,17 @@
 
 import { Box, Typography, styled, Divider } from '@mui/material';
 
-const Component =styled(Box) ({
+const Component = styled(Box)({
     display: 'flex',
     alignItems: 'center',
     flexDirection: 'column',
-    marginTop: 50,
+    margin: 'auto',
     opacity: '0.8',
-    width: '100%'
+    width: '100%',
+    height: '100%'
 });
 
-const StyledDivider = styled(Divider) ({
+const StyledDivider = styled(Divider)({
     width: '100%',
     marginTop: 10
 })
